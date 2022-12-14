882844a94f570de3bc69667e70e83f9b52add8c1
Tamerlan9129 <tamerlankha@code.edu.az>
2022-12-14 18:26:33 +0400

    Add project files.

---
+++ Business/Validators/Category/CategoryCreateDTOValidator.cs
@@ -0,0 +1,20 @@
+﻿using Business.DTOs.Category.Request;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Validators.Category
+{
+    internal class CategoryCreateDTOValidator : AbstractValidator<CategoryCreateDTO>
+    {
+        public CategoryCreateDTOValidator()
+        {
+            RuleFor(x => x.Title)
+                .MinimumLength(3)
+                .WithMessage("Title length must be 3 character minimum");
+        }
+    }
+}
---
+++ Business/Validators/Category/CategoryUpdateDTOValidator.cs
@@ -0,0 +1,20 @@
+﻿using Business.DTOs.Category.Request;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Validators.Category
+{
+    public class CategoryUpdateDTOValidator : AbstractValidator<CategoryUpdateDTO>
+    {
+        public CategoryUpdateDTOValidator()
+        {
+            RuleFor(x => x.Title)
+                .MinimumLength(3)
+                .WithMessage("Title length must be 3 character minimum");
+        }
+    }
+}
